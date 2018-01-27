7da6a5d107a8c52bc9b065fa55cbb7aaea706deb
kollie <zikollie@gmail.com>
2018-01-27 11:12:56 +0000

    Add search functionality to finance section

--- src/financials/GradeTwelve.js
+++ src/financials/GradeTwelve.js
@@ -4,20 +4,62 @@
 import React from 'react'
 import converter from 'number-to-words';
 import ProfileTool from '../elements/ProfileTool'
+import Search from '../elements/Search'
 
-export default function GradeTwo (props) {
-  let {closeProfile} = props
-  const complete = props.studentDetails.filter(studentDetail => !!studentDetail.schoolInfo && !!studentDetail.complete && studentDetail.schoolInfo.class === 'Grade Twelve')
+export default class GradeTwo extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      studentDetails: props.studentDetails,
+      filteredInfo: props.studentDetails
+    }
+    this.handleDataSearch = this.handleDataSearch.bind(this)
+  }
 
-  return (
-    <div className='student-profile'>
-      <div className='student-data'>
-        <ProfileTool closeProfile={closeProfile} />
-        <h4>Student Profile</h4>
-        <GradeStudents limit={1} docs={complete} {...props} />
+  handleDataSearch (data) {
+    let result = this.state.studentDetails
+    const stringContains = (haystack, needle) => {
+      return haystack ? haystack.toLowerCase().includes(needle.toLowerCase()) : false
+    }
+
+    if (data.word.length > 0) {
+      result = result.filter((studentDetail) => {
+        if (stringContains(studentDetail.schoolInfo.studentID, data.word) ||
+          stringContains(studentDetail.schoolInfo.firstname, data.word) ||
+          stringContains(studentDetail.schoolInfo.middlename, data.word) ||
+          stringContains(studentDetail.schoolInfo.lastname, data.word) ||
+          stringContains(studentDetail.schoolInfo.class, data.word) ||
+          stringContains(studentDetail.schoolInfo.enrollmentStatus, data.word)) {
+          return true
+        }
+        return false
+      })
+    }
+    this.setState({filteredInfo: result})
+  }
+
+  componentWillReceiveProps (nextProps) {
+    this.setState({filteredInfo: nextProps.studentDetails})
+  }
+
+  render() {
+    let props = this.props;
+    const filteredInfo = this.state.filteredInfo
+    let {closeProfile} = props
+    const complete = filteredInfo.filter(studentDetail => !!studentDetail.schoolInfo && !!studentDetail.complete && studentDetail.schoolInfo.class === 'Grade Twelve')
+
+    return (
+      <div className='student-profile'>
+        <div className='student-data'>
+          <ProfileTool closeProfile={closeProfile} />
+          <h4>Student Profile</h4>
+          <Search onSearch={this.handleDataSearch} />
+          <hr />
+          <GradeStudents limit={1} docs={complete} {...props} />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }  
 }
 
 export class GradeStudents extends React.Component {
